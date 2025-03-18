644cf946c7312aab079f1f3287d3b3274af511bb
anymz7 <anamunozmunoz509@gmail.com>
2025-03-18 16:53:40 -0500

    Tienda Ecológica
    Carpeta de Clion

---
+++ TiendaEcologica/src/Tienda.cpp
@@ -0,0 +1,15 @@
+//
+// Created by PC on 18/03/2025.
+//
+#include <iostream>
+#include "Tienda.h"
+
+using std::cin;
+using std::cout;
+using std::string
+
+class Tienda {
+    private:
+    
+}
+
---
+++ TiendaEcologica/src/Tienda.h
@@ -0,0 +1,16 @@
+//
+// Created by PC on 18/03/2025.
+//
+
+#ifndef TIENDA_H
+#define TIENDA_H
+
+
+
+class Tienda {
+
+};
+
+
+
+#endif //TIENDA_H
